01033b9625fdb5cf3c947e4b67d59a66f15cca02
AlexTuan1024 <alextuan1024@outlook.com>
2016-11-28 20:25:54 +0800

    Revert "1.validate number"
    This reverts commit 5535cab3c1f5145e74931824ff9b8ef90b0d7356.

--- tutorial02/leptjson.c
+++ tutorial02/leptjson.c
@@ -1,7 +1,5 @@
 #include "leptjson.h"
 #include <assert.h> /* assert() */
-#include <errno.h>  /* errno, ERANGE */
-#include <math.h>   /* HUGE_VAL */
 #include <stdlib.h> /* NULL, strtod() */
 
 #define EXPECT(c, ch)                                                          \
@@ -9,8 +7,6 @@
     assert(*c->json == (ch));                                                  \
     c->json++;                                                                 \
   } while (0)
-#define ISDIGIT(ch) ((ch) >= '0' && (ch) <= '9')
-#define ISDIGIT1TO9(ch) ((ch) >= '1' && (ch) <= '9')
 
 typedef struct { const char *json; } lept_context;
 
@@ -62,39 +58,14 @@ static int lept_parse_literal(lept_context *c, lept_value *v,
 }
 
 static int lept_parse_number(lept_context *c, lept_value *v) {
-  const char *p = c->json;
-  if (*p == '-')
-    p++;
-  if (*p == '0')
-    p++;
-  else {
-    if (!ISDIGIT1TO9(*p))
-      return LEPT_PARSE_INVALID_VALUE;
-    for (p++; ISDIGIT(*p); p++)
-      ;
-  }
-  if (*p == '.') {
-    p++;
-    if (!ISDIGIT(*p))
-      return LEPT_PARSE_INVALID_VALUE;
-    for (p++; ISDIGIT(*p); p++)
-      ;
-  }
-  if (*p == 'e' || *p == 'E') {
-    p++;
-    if (*p == '+' || *p == '-')
-      p++;
-    if (!ISDIGIT(*p))
-      return LEPT_PARSE_INVALID_VALUE;
-    for (p++; ISDIGIT(*p); p++)
-      ;
-  }
-  errno = 0;
-  v->n = strtod(c->json, NULL);
-  if (errno == ERANGE && (v->n == HUGE_VAL || v->n == -HUGE_VAL))
-    return LEPT_PARSE_NUMBER_TOO_BIG;
+  char *end;
+  // TODO: It is legal that the first valid character is '+' in C,but it's not
+  // valid in JSON
+  v->n = strtod(c->json, &end);
+  if (c->json == end)
+    return LEPT_PARSE_INVALID_VALUE;
+  c->json = end;
   v->type = LEPT_NUMBER;
-  c->json = p;
   return LEPT_PARSE_OK;
 }
 
